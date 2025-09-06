33643f86ea7d2d1053c89a7cfe885c6430c9f160
Atharva Kadam <kadamatharva04@gmail.com>
2025-09-06 22:40:45 +0530

    Create static.js

---
+++ routes/static.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const URL = require('../models/url');
+
+router.get('/', async (req, res) => {
+    if (!req.user) return res.redirect('/login')
+    const allurls = await URL.find({ createdBy: req.user._id });
+    return res.render('home', {
+        urls: allurls,
+    });
+});
+router.get('/welcome', (req, res) => {
+    return res.render('Welcome');
+})
+
+router.get('/signup', (req, res) => {
+    return res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    return res.render('login');
+})
+
+module.exports = router;
